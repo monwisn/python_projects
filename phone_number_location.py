93ae74064574c99147e3463c4ee67a21a0b6225e
monwisn <48805011+monwisn@users.noreply.github.com>
2022-07-05 10:32:48 +0200

    Add files via upload

---
+++ phone_number_location.py
@@ -0,0 +1,10 @@
+import phonenumbers
+
+from phonenumbers import geocoder
+
+phone_number = phonenumbers.parse("Number with country code")
+# for example: Area code for Poland: +48 XXX XXX XXX
+# phone_number = phonenumbers.parse("+48123456789")
+
+# this will print the country name
+print(geocoder.description_for_number(phone_number, 'en'))
